95ac2d8246a668f1590a538628001dda99730429
Mooo282 <mohpro1122@hotmail.com>
2026-03-20 14:12:48 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -158,4 +158,10 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('Server running on 3000'));
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`PixDeception is running on port ${PORT}`);
+});
+
