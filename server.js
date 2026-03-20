0e423b29f7b3c5bc6d2c8090bc23ee819b016c02
Mooo282 <mohpro1122@hotmail.com>
2026-03-20 09:19:37 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-// --- نظام الباقات (Image Packs) ---
+// --- Image Packs Setup ---
 const imagePools = {
-    "classic": ["/images/classic/1.jpg", "/images/classic/2.jpg", "/images/classic/3.jpg", "/images/classic/4.jpg", "/images/classic/5.jpg", "/images/classic/6.jpg", "/images/classic/7.jpg", "/images/classic/8.jpg", "/images/classic/9.jpg", "/images/classic/10.jpg"],
-    "anime": ["/images/anime/1.jpg", "/images/anime/2.jpg", "/images/anime/3.jpg", "/images/anime/4.jpg", "/images/anime/5.jpg", "/images/anime/6.jpg"],
-    "nature": ["/images/nature/1.jpg", "/images/nature/2.jpg", "/images/nature/3.jpg", "/images/nature/4.jpg", "/images/nature/5.jpg", "/images/nature/6.jpg"]
+    "classic": ["/images/classic/1.jpg", "/images/classic/2.jpg", "/images/classic/3.jpg", "/images/classic/4.jpg", "/images/classic/5.jpg", "/images/classic/6.jpg"],
+    "fun": ["/images/fun/1.jpg", "/images/fun/2.jpg", "/images/fun/3.jpg", "/images/fun/4.jpg", "/images/fun/5.jpg", "/images/fun/6.jpg"]
 };
 
 let currentPool = [];
@@ -19,7 +18,7 @@ let players = [], scores = {}, playerNames = {}, hostId = null;
 let targetPoints = 30, roundTimeLimit = 60, currentDrawerId = null;
 let gameState = "LOBBY", currentImages = [], currentClue = "", correctImage = "";
 let fakeImages = {}, votes = {}, guessesReceived = 0, timer, timeLeft = 60;
-let socketToUserId = {}, drawerQueue = [], disconnectTimeouts = {};
+let socketToUserId = {}, drawerQueue = [], drawerGraceTimer = null;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
@@ -41,16 +40,17 @@ function startTimer(duration, onTimeout) {
 io.on('connection', (socket) => {
     socket.on('joinGame', (data) => {
         const uId = data.userId;
-        if (disconnectTimeouts[uId]) { clearTimeout(disconnectTimeouts[uId]); delete disconnectTimeouts[uId]; }
+        if (uId === currentDrawerId && drawerGraceTimer) {
+            clearTimeout(drawerGraceTimer);
+            drawerGraceTimer = null;
+        }
         socketToUserId[socket.id] = uId;
         playerNames[uId] = data.name;
         if (scores[uId] === undefined) scores[uId] = 0;
         if (!players.includes(uId)) players.push(uId);
         if (!hostId || !players.includes(hostId)) hostId = uId;
-        
         emitPlayerList();
 
-        // استعادة الحالة عند الريفرش
         if (gameState !== "LOBBY") {
             socket.emit('roundStarted', { images: currentImages, drawerId: currentDrawerId, drawerName: playerNames[currentDrawerId], targetPoints });
             if (currentClue) socket.emit('showClue', { clue: currentClue, pImages: [], drawerName: playerNames[currentDrawerId] });
@@ -61,11 +61,17 @@ io.on('connection', (socket) => {
         }
     });
 
+    socket.on('sendChat', (msg) => {
+        const uId = socketToUserId[socket.id];
+        if (msg) io.emit('newChat', { sender: playerNames[uId], text: msg });
+    });
+
     socket.on('requestStart', (data) => {
         if (socketToUserId[socket.id] === hostId && gameState === "LOBBY") {
             players.forEach(id => scores[id] = 0);
-            targetPoints = parseInt(data.targetPoints) || 30;
-            roundTimeLimit = parseInt(data.roundTime) || 60;
+            emitPlayerList();
+            targetPoints = parseInt(data.targetPoints);
+            roundTimeLimit = parseInt(data.roundTime);
             currentPool = imagePools[data.mode] || imagePools["classic"];
             drawerQueue = [];
             startNewRound();
@@ -76,8 +82,6 @@ io.on('connection', (socket) => {
         gameState = "DRAWING"; guessesReceived = 0; fakeImages = {}; votes = {}; currentClue = "";
         if (drawerQueue.length === 0) drawerQueue = [...players].sort(() => 0.5 - Math.random());
         currentDrawerId = drawerQueue.shift();
-        if (!players.includes(currentDrawerId)) return players.length > 0 ? startNewRound() : finishGame();
-
         currentImages = [...currentPool].sort(() => 0.5 - Math.random()).slice(0, 6);
         io.emit('roundStarted', { images: currentImages, drawerId: currentDrawerId, drawerName: playerNames[currentDrawerId], targetPoints });
         startTimer(roundTimeLimit, () => { if(gameState === "DRAWING") startNewRound(); });
@@ -121,10 +125,8 @@ io.on('connection', (socket) => {
         let totalVoters = players.length - 1;
         let correctCount = 0;
         for (let vId in votes) if (votes[vId] === correctImage) correctCount++;
-
         if (correctCount > 0 && correctCount < totalVoters) {
             scores[currentDrawerId] += (correctCount * 2);
-            for (let vId in votes) if (votes[vId] === correctImage) scores[vId] += 3;
         }
         for (let vId in votes) {
             for (let fId in fakeImages) if (votes[vId] === fakeImages[fId] && fId !== vId) scores[fId] += 1;
@@ -140,7 +142,6 @@ io.on('connection', (socket) => {
         }
         for (let fId in fakeImages) fakers[fakeImages[fId]] = playerNames[fId];
         io.emit('roundFinished', { correctImage, scores, voteDetails, fakers, drawerName: playerNames[currentDrawerId] });
-        
         setTimeout(() => {
             if (players.some(id => scores[id] >= targetPoints)) finishGame();
             else if (players.length > 0) startNewRound();
@@ -156,14 +157,14 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const uId = socketToUserId[socket.id];
         if (uId) {
-            disconnectTimeouts[uId] = setTimeout(() => {
-                players = players.filter(id => id !== uId);
-                if (uId === currentDrawerId) startNewRound();
-                delete playerNames[uId]; delete scores[uId];
-                emitPlayerList();
-            }, 10000);
+            if (uId === currentDrawerId && gameState !== "LOBBY") {
+                drawerGraceTimer = setTimeout(() => {
+                    if (gameState !== "LOBBY") startNewRound();
+                }, 10000);
+            }
+            delete socketToUserId[socket.id];
         }
     });
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(3000, () => console.log('PixDeception: Server Version running on port 3000'));
