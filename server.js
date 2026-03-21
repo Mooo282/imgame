03774a52d1e2be43aa6071332219fb2f8c87c4b9
Mooo282 <mohpro1122@hotmail.com>
2026-03-21 08:40:21 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path'); // مطلوب لإدارة المسارات
+const path = require('path'); 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-// إعداد المسارات بناءً على هيكلة ملفاتك
-// الوصول للمجلد الذي يحتوي على الصور
+
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
-// تقديم ملف index.html من المجلد الرئيسي
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
